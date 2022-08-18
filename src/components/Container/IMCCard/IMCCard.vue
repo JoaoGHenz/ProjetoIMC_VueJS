9b8a168c10ccffd851279710b7e7aac6ae5131bb
Jao <joao.henz.pb@compasso.com.br>
2022-08-18 16:21:38 -0300

    Feat: text component and sliders

--- src/components/Container/IMCCard/IMCCard.vue
+++ src/components/Container/IMCCard/IMCCard.vue
@@ -1,16 +1,26 @@
 <template>
     <div id="IMCCard">
         <TitleComponent content="Calcule seu IMC"/>
+        <div class="firstSlide">
+            <TextComponent text="Peso"/>
+            <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+        </div>
+        <div class="secondSlide">
+            <TextComponent text="Altura"/>
+            <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+        </div>
     </div>
 </template>
 
 <script>
 import TitleComponent from '@/components/Micro/Title/TitleComponent.vue';
+import TextComponent from '@/components/Micro/Text/TextComponent.vue';
 
 export default {
     name: "IMCCard",
     components: {
     TitleComponent,
+    TextComponent,
 }
 }
 
@@ -23,6 +33,10 @@ export default {
     height: 50%;
     background: rgba(255, 255, 255, 0.6);
     border-radius: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-flow: column wrap;
 }
 
 </style>
---
+++ src/components/Micro/Text/TextComponent.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <p>
+        {{ text }}
+    </p>
+
+</template>
+
+<script>
+
+export default {
+    name: "TextComponent",
+    props: {
+        text: {
+            String,
+            default: "Text Component"
+        }
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+p {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 300;
+    font-size: 24px;
+    line-height: 36px;
+}
+
+</style>
